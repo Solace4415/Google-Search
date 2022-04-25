c49e4c41f773491a9e3b079d73631e005b948987
unknown <tijjtay@gmail.com>
2022-04-25 22:52:13 +0100

    done

---
+++ src/components/Routess.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Results from "./Results";
+
+const array = ["/search", "/image", "/news", "/video"];
+
+const Routess = () => {
+  return (
+    <div className="p-4">
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/search" />} />
+
+        {array.map((path) => (
+          <Route exact key={path} path={path} element={<Results />} />
+        ))}
+      </Routes>
+    </div>
+  );
+};
+
+export default Routess;
---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("ELon");
+
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type} `, {
+      method: "GET",
+      headers: {
+        "X-User-Agent": "desktop",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        "X-RapidAPI-Key": "481e8a60ecmshd74473b2c6fc7f6p158f6fjsnc80416c12b97",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log({ type, data });
+
+    if (type.includes("/news")) {
+      setResults(data.entries);
+    } else if (type.includes("/image")) {
+      setResults(data.image_results);
+    } else {
+        setResults(data.results);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
